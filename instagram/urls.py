6ffbca516d64f00f33414a43c1a34762a56f1cca
mbeatrice1 <mahorobeatrice9@gmail.com>
2021-04-23 22:54:11 +0200

    added models

--- instagram/urls.py
+++ instagram/urls.py
@@ -15,7 +15,14 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.contrib.auth import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'',include('gram.urls')),
+    url(r'^accounts/', include('registration.backends.simple.urls')),
+    url(r'^login/$', auth_views.login, name='login'),
+    url(r'^logout/$', views.logout, {"next_page": '/'}),
+]
 ]
