dfce2f3aa3554fa5f24c933a8f923abb9d604dbf
mbeatrice1 <mahorobeatrice9@gmail.com>
2021-04-26 07:41:22 +0200

    modified the  requirements

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/bea/Documents/Python/IPS/Django_Framework_projects/IP2/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
